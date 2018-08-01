2d652316c454813f1f9702f5af83527c12955f09
Matthew Tai <matthewtai@gmail.com>
2018-08-01 02:17:37 -0400

    tweak

--- liri.js
+++ liri.js
@@ -8,85 +8,89 @@ var spotify = new Spotify(keys.spotify);
 var argumentArr = process.argv.slice(3);
 var input = argumentArr.join(" ");
 
-function spotifySearch(){
+function spotifySearch() {
 
-  spotify.search({ type: 'track', query: input, limit: '1' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-    console.log("\n------------------------------------------------------");
-    console.log(`Artist: ${data.tracks.items[0].artists[0].name} Song: ${data.tracks.items[0].name}\nAlbum: ${data.tracks.items[0].album.name}\nURL: ${data.tracks.items[0].preview_url}`);
-    console.log("------------------------------------------------------\n");
-  });
-}
-
-function movieSearch(){
-    
-var request = require("request");
-
-request("http://www.omdbapi.com/?t="+ input +"&y=&plot=short&apikey=bbf8c15", function(error, response, body) {
-    var omdb = JSON.parse(body);
-if(omdb.Title === undefined){
-    console.log("\n------------------------");
-    console.log("Movie Does Not Exist");
-    console.log("------------------------\n");
-}else if(omdb.Ratings.length < 1){
-    var rottenTomato = "";
-}
-if (!error && response.statusCode === 200) {
-        
-    
+    spotify.search({
+        type: 'track',
+        query: input,
+        limit: '1'
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
         console.log("\n------------------------------------------------------");
-        console.log(`Title: ${omdb.Title}\nYear: ${omdb.Year}\nImdb Rating: ${omdb.imdbRating}\nRotten Tomatoes Rating: ${omdb.Ratings[1].Value}\nCountry: ${omdb.Country}\nPlot: ${omdb.Plot}\nActors: ${omdb.Actors}`);
-        console.log("------------------------------------------------------\n"); 
-    }
-  });
+        console.log(`Artist: ${data.tracks.items[0].artists[0].name} Song: ${data.tracks.items[0].name}\nAlbum: ${data.tracks.items[0].album.name}\nURL: ${data.tracks.items[0].preview_url}`);
+        console.log("------------------------------------------------------\n");
+    });
+}
+
+function movieSearch() {
+
+    var request = require("request");
+
+    request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=bbf8c15", function (error, response, body) {
+        var omdb = JSON.parse(body);
+        if (omdb.Title === undefined) {
+            console.log("\n------------------------");
+            console.log("Movie Does Not Exist");
+            console.log("------------------------\n");
+        } else if (omdb.Ratings.length < 1) {
+            var rottenTomato = "";
+        }
+        if (!error && response.statusCode === 200) {
+
+
+            console.log("\n------------------------------------------------------");
+            console.log(`Title: ${omdb.Title}\nYear: ${omdb.Year}\nImdb Rating: ${omdb.imdbRating}\nRotten Tomatoes Rating: ${omdb.Ratings[1].Value}\nCountry: ${omdb.Country}\nPlot: ${omdb.Plot}\nActors: ${omdb.Actors}`);
+            console.log("------------------------------------------------------\n");
+        }
+    });
 }
 
 
-function randomFile(){
-fs.readFile("random.txt", "utf8", function(error, data) {
+function randomFile() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
 
-    if (error) {
-      return console.log(error);
-    }
-  
-    var dataArr = data.split(",");
+        if (error) {
+            return console.log(error);
+        }
 
-    for(var i=0; i < dataArr.length; i++){
+        var dataArr = data.split(",");
 
-    }
+        for (var i = 0; i < dataArr.length; i++) {
 
-    if(dataArr[0] === "spotify-this-song"){
-        input = dataArr[1];
-        spotifySearch();
-    }
+        }
 
-    if(dataArr[0] === "movie-this"){
-        input = dataArr[1];
-        movieSearch();
-    }
+        if (dataArr[0] === "spotify-this-song") {
+            input = dataArr[1];
+            spotifySearch();
+        }
 
-    
-  
-  });
+        if (dataArr[0] === "movie-this") {
+            input = dataArr[1];
+            movieSearch();
+        }
+
+
+
+    });
 }
-  
-if(process.argv[2] === "spotify-this-song" && process.argv[3] == null || process.argv[2] === "spotify-this-song" && process.argv[3] === ""){
-   var input = "Ace of Base";
-   spotifySearch();
 
-}else if(process.argv[2] === "movie-this" && process.argv[3] == null || process.argv[2] === "movie-this" && process.argv[3] === ""){
+if (process.argv[2] === "spotify-this-song" && process.argv[3] == null || process.argv[2] === "spotify-this-song" && process.argv[3] === "") {
+    var input = "Ace of Base";
+    spotifySearch();
+
+} else if (process.argv[2] === "movie-this" && process.argv[3] == null || process.argv[2] === "movie-this" && process.argv[3] === "") {
     var input = "Mr. Nobody";
     movieSearch();
 
-}else if(process.argv[2] === "spotify-this-song"){
-spotifySearch();
+} else if (process.argv[2] === "spotify-this-song") {
+    spotifySearch();
 
-}else if(process.argv[2] === "movie-this"){
-movieSearch();
+} else if (process.argv[2] === "movie-this") {
+    movieSearch();
 
-}else if(process.argv[2] === "do-what-it-says"){
-randomFile();
+} else if (process.argv[2] === "do-what-it-says") {
+    randomFile();
 
 }
